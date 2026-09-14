5748c290ba1391e885edb9a72eb43d6bd8dff85e
Gwon JunHyeon <itsjunwtr@gmail.com>
2026-09-14 21:54:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// ===== BGM 재생 =====
+const playlist = [
+  'music/music.mp3'   // 노래 추가하면 여기에 파일명 넣기
+];
 
+let currentIndex = 0;
+const audio = new Audio();
+audio.volume = 1.0;
+
+audio.addEventListener('ended', () => {
+  currentIndex = (currentIndex + 1) % playlist.length;
+  audio.src = playlist[currentIndex];
+  audio.play();
+});
+
+document.getElementById('playBtn').addEventListener('click', () => {
+  audio.src = playlist[currentIndex];
+  audio.play();
+});
