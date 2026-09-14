e7cc3113b2016033b5a5c62254b4e035e334f839
Gwon JunHyeon <itsjunwtr@gmail.com>
2026-09-14 22:04:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ===== BGM 재생 =====
 const playlist = [
-  'music/SekaiwaKoiNiOchiteiruInst.mp3',   // 노래 추가하면 여기에 파일명 넣기
+  'music/Sekaiwa_Koi_Ni_Ochiteiru_Inst.mp3',
   'music/toYouMyLight.mp3'
 ];
 
@@ -8,13 +8,21 @@ let currentIndex = 0;
 const audio = new Audio();
 audio.volume = 1.0;
 
+// 곡이 끝나면 다음 곡 재생 (무한 반복)
 audio.addEventListener('ended', () => {
   currentIndex = (currentIndex + 1) % playlist.length;
   audio.src = playlist[currentIndex];
   audio.play();
 });
 
-document.getElementById('playBtn').addEventListener('click', () => {
+// 첫 재생 시작
+function startMusic() {
   audio.src = playlist[currentIndex];
   audio.play();
-});
+  document.removeEventListener('click', startMusic);
+  document.removeEventListener('touchstart', startMusic);
+}
+
+// 화면 아무데나 클릭/터치하면 재생
+document.addEventListener('click', startMusic);
+document.addEventListener('touchstart', startMusic);
